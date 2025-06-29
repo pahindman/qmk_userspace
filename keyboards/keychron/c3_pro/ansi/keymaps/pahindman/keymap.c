1093c993bfa0ff2c42609fc07598d66012473cc1
Phil Hindman <pahindman@user.noreply.github.com>
2025-06-29 17:40:23 -0500

    Add LT1_ESC macro to make keymaps easier to read

--- keyboards/keychron/c3_pro/ansi/keymaps/pahindman/keymap.c
+++ keyboards/keychron/c3_pro/ansi/keymaps/pahindman/keymap.c
@@ -18,6 +18,7 @@
 
 #define LCTL_Z  LCTL_T(KC_Z)
 #define RCTL_SL RCTL_T(KC_SLSH)
+#define LT1_ESC  LT(1, KC_ESC)
 
 #if defined(KEYBOARD_keychron_c3_pro_ansi_red)
 #define C3P_HUI _______
@@ -53,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,                    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_SCRL,  KC_PAUS,
         KC_GRV,          KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,
         KC_TAB,          KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN,
-        LT(1, KC_ESC),   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,
+        LT1_ESC,         KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,
         KC_LSFT,                   LCTL_Z,   KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   RCTL_SL,  KC_RSFT,                      KC_UP,
         KC_LCTL,         KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(1),    KC_RWIN,  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
