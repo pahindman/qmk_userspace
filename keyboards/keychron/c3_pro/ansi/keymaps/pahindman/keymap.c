b883b45e10892fad6de7d15f3cf10b2168012305
Phil Hindman <pahindman@user.noreply.github.com>
2025-06-29 17:47:49 -0500

    Formatting: remove unnecessary space on each line

--- keyboards/keychron/c3_pro/ansi/keymaps/pahindman/keymap.c
+++ keyboards/keychron/c3_pro/ansi/keymaps/pahindman/keymap.c
@@ -51,34 +51,34 @@
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_tkl_ansi(
-        KC_ESC,                    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_SCRL,  KC_PAUS,
-        KC_GRV,          KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,
-        KC_TAB,          KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN,
-        LT1_ESC,         KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,
-        KC_LSFT,                   LCTL_Z,   KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   RCTL_SL,  KC_RSFT,                      KC_UP,
-        KC_LCTL,         KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(1),    KC_RWIN,  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_ESC,             KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_SCRL,  KC_PAUS,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN,
+        LT1_ESC,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,
+        KC_LSFT,            LCTL_Z,   KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   RCTL_SL,  KC_RSFT,                      KC_UP,
+        KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(1),    KC_RWIN,  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [1] = LAYOUT_tkl_ansi(
-        QK_BOOT,                   KC_BRID,  KC_BRIU,  _______,  _______,  C3P_VAD,  C3P_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  DT_PRNT,  _______,  C3P_TOG,
-        EE_CLR,          _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  DT_UP,
-        BL_TOGG,         C3P_MOD,  C3P_VAI,  C3P_HUI,  C3P_SAI,  C3P_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  DT_DOWN,
-        _______,         C3P_RMOD, C3P_VAD,  C3P_HUD,  C3P_SAD,  C3P_SPD,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  _______,  _______,  _______,
-        _______,                   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,
-        _______,         _______,  _______,                                _______,                                DF(2),    _______,  KC_APP,   _______,  _______,  _______,  _______),
+        QK_BOOT,            KC_BRID,  KC_BRIU,  _______,  _______,  C3P_VAD,  C3P_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  DT_PRNT,  _______,  C3P_TOG,
+        EE_CLR,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  DT_UP,
+        BL_TOGG,  C3P_MOD,  C3P_VAI,  C3P_HUI,  C3P_SAI,  C3P_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  DT_DOWN,
+        _______,  C3P_RMOD, C3P_VAD,  C3P_HUD,  C3P_SAD,  C3P_SPD,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  _______,  _______,  _______,
+        _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,
+        _______,  _______,  _______,                                _______,                                DF(2),    _______,  KC_APP,   _______,  _______,  _______,  _______),
 
     [2] = LAYOUT_tkl_ansi(
-        KC_ESC,                    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_SCRL,  KC_PAUS,
-        KC_GRV,          KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,
-        KC_TAB,          KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN,
-        KC_CAPS,         KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,
-        KC_LSFT,                   KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,                      KC_UP,
-        KC_LCTL,         KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(3),    KC_RWIN,  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_ESC,             KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_SCRL,  KC_PAUS,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN,
+        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,
+        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,                      KC_UP,
+        KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(3),    KC_RWIN,  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [3] = LAYOUT_tkl_ansi(
-        _______,                   KC_BRID,  KC_BRIU,  _______,  _______,  C3P_VAD,  C3P_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,  _______,  C3P_TOG,
-        _______,         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-        BL_TOGG,         C3P_MOD,  C3P_VAI,  C3P_HUI,  C3P_SAI,  C3P_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-        _______,         C3P_RMOD, C3P_VAD,  C3P_HUD,  C3P_SAD,  C3P_SPD,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-        _______,                   _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,  _______,                      _______,
-        _______,         _______,  _______,                                _______,                                DF(0),    _______,  KC_APP,   _______,  _______,  _______,  _______),
+        _______,            KC_BRID,  KC_BRIU,  _______,  _______,  C3P_VAD,  C3P_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,  _______,  C3P_TOG,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        BL_TOGG,  C3P_MOD,  C3P_VAI,  C3P_HUI,  C3P_SAI,  C3P_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  C3P_RMOD, C3P_VAD,  C3P_HUD,  C3P_SAD,  C3P_SPD,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,            _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,  _______,                      _______,
+        _______,  _______,  _______,                                _______,                                DF(0),    _______,  KC_APP,   _______,  _______,  _______,  _______),
 };
